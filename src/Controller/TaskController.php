0e618598753b4f2e497819dce85a1cf69d72e8a9
Elhadj BAH <ismailabah16@gmail.com>
2022-03-19 21:44:49 +0100

    suppession deprecation et correction code par rapport à la version 5.4 - ainsi que l'affichage de la liste des taches sur twig

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,36 +4,35 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Form\TaskType;
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/tasks", name="task_list" ,methods={"GET","POST"})
+     * @Route("/tasks", name="task_list" ,methods={"GET"})
      */
-    public function listAction()
+    public function listAction(TaskRepository $taskRepository):Response
     {
-        return $this->render('task/list.html.twig', ['tasks' => $this->getDoctrine()->getRepository('AppBundle:Task')->findAll()]);
+        return $this->render('task/list.html.twig',
+            ['tasks' =>$taskRepository ->findAll()]);
     }
 
     /**
      * @Route("/tasks/create", name="task_create" ,methods={"GET","POST"})
      */
-    public function createAction(Request $request)
+    public function createAction(Request $request, TaskRepository $taskRepository)
     {
         $task = new Task();
         $form = $this->createForm(TaskType::class, $task);
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-
-            $em->persist($task);
-            $em->flush();
-
+        if ($form->isSubmitted()&& $form->isValid()) {
+            $taskRepository->add($task);
             $this->addFlash('success', 'La tâche a été bien été ajoutée.');
 
             return $this->redirectToRoute('task_list');
@@ -45,14 +44,14 @@ class TaskController extends AbstractController
     /**
      * @Route("/tasks/{id}/edit", name="task_edit" ,methods={"GET","POST"})
      */
-    public function editAction(Task $task, Request $request)
+    public function editAction(Task $task, Request $request, TaskRepository $taskRepository)
     {
         $form = $this->createForm(TaskType::class, $task);
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+        if ($form->isSubmitted()&&$form->isValid()) {
+            $taskRepository->add($task);
 
             $this->addFlash('success', 'La tâche a bien été modifiée.');
 
@@ -68,10 +67,10 @@ class TaskController extends AbstractController
     /**
      * @Route("/tasks/{id}/toggle", name="task_toggle" ,methods={"GET","POST"})
      */
-    public function toggleTaskAction(Task $task)
+    public function toggleTaskAction(Task $task, TaskRepository $taskRepository)
     {
         $task->toggle(!$task->isDone());
-        $this->getDoctrine()->getManager()->flush();
+        $taskRepository->add($task);
 
         $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
 
@@ -79,13 +78,13 @@ class TaskController extends AbstractController
     }
 
     /**
-     * @Route("/tasks/{id}/delete", name="task_delete" ,methods={"GET","DELETE"})
+     * @Route("/tasks/{id}/delete", name="task_delete" ,methods={"POST"})
      */
-    public function deleteTaskAction(Task $task)
+    public function deleteTaskAction(Task $task, Request $request, TaskRepository $taskRepository)
     {
-        $em = $this->getDoctrine()->getManager();
-        $em->remove($task);
-        $em->flush();
+        if($this->isCsrfTokenValid('delete'.$task->getId(),$request->request->get('_token'))){
+            $taskRepository->remove($task);
+        }
 
         $this->addFlash('success', 'La tâche a bien été supprimée.');
 
