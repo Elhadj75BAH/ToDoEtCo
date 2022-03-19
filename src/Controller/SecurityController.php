17e03d69aeb88cf3d21c4365620ad8c79ee7b514
Elhadj BAH <ismailabah16@gmail.com>
2022-03-19 14:56:52 +0100

    mise en place des controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login",methods={"GET","POST"})
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/login_check", name="login_check" ,methods={"GET","POST"})
+     */
+    public function loginCheck()
+    {
+        // This code is never executed.
+    }
+
+    /**
+     * @Route("/logout", name="logout" ,methods={"GET","POST"})
+     */
+    public function logoutCheck()
+    {
+        // This code is never executed.
+    }
+}
