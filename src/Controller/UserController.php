703583f7b485ca7b7c8d9fd1ecd1b9b624488556
Elhadj BAH <ismailabah16@gmail.com>
2022-03-23 14:27:08 +0100

    gestion de la  connection  des utilisateur

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,16 +7,16 @@ use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
-    private $encoder;
-    public function __construct(UserPasswordEncoderInterface $encoder)
+
+    public function __construct()
     {
-        $this->encoder=$encoder;
+
     }
 
     /**
@@ -32,7 +32,9 @@ class UserController extends AbstractController
     /**
      * @Route("/users/create", name="user_create")
      */
-    public function createAction(Request $request, UserRepository $userRepository)
+    public function createAction(Request $request,
+                                 UserRepository $userRepository,
+                                 UserPasswordHasherInterface $userPasswordHasher)
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
@@ -41,8 +43,12 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted()&&$form->isValid()) {
 
-            $password = $this->encoder->encodePassword($user,'password');
-            $user->setPassword($password);
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
             $userRepository->add($user);
 
             $this->addFlash('success', "L'utilisateur a bien été ajouté.");
@@ -58,15 +64,23 @@ class UserController extends AbstractController
     /**
      * @Route("/users/{id}/edit", name="user_edit")
      */
-    public function editAction(User $user, Request $request)
+    public function editAction(User $user,
+                               Request $request,
+                               UserPasswordHasherInterface $userPasswordHasher,
+                               UserRepository $userRepository)
     {
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted()&& $form->isValid()) {
-            $password = $this->encoder->encodePassword($user,'password');
-            $user->setPassword($password);
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $userRepository->add($user);
 
             $this->addFlash('success', "L'utilisateur a bien été modifié");
 
