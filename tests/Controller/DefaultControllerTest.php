c268e93d38d96ab6af15510f8eaaf601e0f7f426
Elhadj BAH <ismailabah16@gmail.com>
2022-05-06 15:37:58 +0200

    controller test default

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $this->assertSelectorTextContains('h1',"Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !");
+
+    }
+}
