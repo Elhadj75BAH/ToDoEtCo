ce0306e32e133ed01d1f83140f3b2b8b59cf175e
Elhadj BAH <ismailabah16@gmail.com>
2022-06-10 11:46:10 +0200

    test creatAction

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -5,8 +5,31 @@ namespace App\Tests\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class UserControllerTest extends WebTestCase
 {
+
+    public function testCreatAction()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/register');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+
+        $client->submitForm('Inscription',[
+            'registration_form[username]'=>'Elhdajbah7',
+            'registration_form[password][first]'=>'Password',
+            'registration_form[password][second]'=>'Password',
+            'registration_form[email]'=>'elhaddjbahtest7@test.fr',
+            'registration_form[agreeTerms]'=>'1',
+        ]);
+        $this->assertResponseRedirects();
+
+       $client->followRedirect();
+    }
+
+
 
 }
