5f2d41045892b4b500935aa05e6bf136949850d8
Elhadj BAH <ismailabah16@gmail.com>
2022-06-10 19:27:19 +0200

    test login creatAction editAction

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -2,8 +2,7 @@
 
 namespace App\Tests\Controller;
 
-
-
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -11,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 class UserControllerTest extends WebTestCase
 {
 
-    public function testCreatAction()
+    public function testRegister()
     {
         $client = static::createClient();
         $client->request('GET', '/register');
@@ -19,15 +18,86 @@ class UserControllerTest extends WebTestCase
 
 
         $client->submitForm('Inscription',[
-            'registration_form[username]'=>'Elhdajbah7',
+            'registration_form[username]'=>'Elhdajbaheeddtt222365',
             'registration_form[password][first]'=>'Password',
             'registration_form[password][second]'=>'Password',
-            'registration_form[email]'=>'elhaddjbahtest7@test.fr',
+            'registration_form[email]'=>'elhaddjbahtebbstttyyeee213423@test.fr',
             'registration_form[agreeTerms]'=>'1',
         ]);
         $this->assertResponseRedirects();
 
        $client->followRedirect();
+    }
+
+
+    public function testLogin(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $this->assertEquals(1, $crawler->filter(
+            'input[name="username"]')->count());
+        $this->assertEquals(1, $crawler->filter(
+            'input[name="password"]')->count());
+        $this->assertEquals(1, $crawler->filter(
+            'input[name="_csrf_token"]')->count());
+
+        $form = $crawler->selectButton('Se connecter')->form();
+
+        $form['username'] = 'Elhdajbah6';
+        $form['password'] = 'password';
+        $client->submit($form);
+
+    }
+
+
+
+    public function testCreateAction(): void
+    {
+
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByUsername('Elhdajbah6');
+        $client->loginUser($testUser);
+
+       $client->request('GET', '/users/create');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $client->submitForm('Ajouter',[
+            'user[username]'=>'sorry_barrrr',
+            'user[password][first]'=>'Password',
+            'user[password][second]'=>'Password',
+            'user[email]'=>'saranbarrrr@test.fr',
+        ]);
+        $this->assertResponseRedirects();
+
+        $client->followRedirect();
+
+    }
+
+
+    public function testEditAction()
+    {
+
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByUsername('Elhdajbah6');
+        $client->loginUser($testUser);
+
+        $client->request('GET', '/users/48/edit');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $client->submitForm('Modifier',[
+            'user[username]'=>'barryyy',
+            'user[password][first]'=>'password',
+            'user[password][second]'=>'password',
+            'user[email]'=>'barry@test.fr',
+        ]);
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
     }
 
 
