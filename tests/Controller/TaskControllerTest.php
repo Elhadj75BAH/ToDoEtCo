359bcc3054dc84a942442f8d3fe5c9f27bfa3bb6
Elhadj BAH <ismailabah16@gmail.com>
2022-07-01 21:52:37 +0200

    test task delete anonymous user-author

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -66,6 +66,10 @@ class TaskControllerTest extends WebTestCase
 
     }
 
+    /**
+     * This is a test for deleting any task by an admin (anonymous task or task user related)
+     * @return void
+     */
 
     public function testDeleteTaskAction()
     {
@@ -75,16 +79,70 @@ class TaskControllerTest extends WebTestCase
         $client->loginUser($testUser);
 
 
-        $client->request('POST', '/tasks/39/delete');
+        $client->request('POST', '/tasks/44/delete');
 
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
 
         $this->assertResponseRedirects();
         /** @var TaskRepository $taskRepository */
         $taskRepository = static ::getContainer()->get(TaskRepository::class);
-        $task = $taskRepository->find(39);
+        $task = $taskRepository->find(44);
         $this->assertNull($task);
 
     }
+
+    /**
+     * This is a test for deleting a task belonging to its user author
+     * @return void
+     */
+
+    public function testDeleteMyTask()
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByUsername('baro');
+        $client->loginUser($testUser);
+
+
+        $client->request('POST', '/tasks/6/delete');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $this->assertResponseRedirects();
+        /** @var TaskRepository $taskRepository */
+        $taskRepository = static ::getContainer()->get(TaskRepository::class);
+        $task = $taskRepository->find(6);
+        $this->assertNull($task);
+
+    }
+
+    /**
+     * A test for the removal of an anonymous task by a user.
+     * This should not be possible
+     * @return void
+     */
+
+    public function testDeleteAnonymeTask()
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByUsername('baro');
+        $client->loginUser($testUser);
+
+
+        $client->request('POST', '/tasks/44/delete');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $this->assertResponseRedirects();
+        /** @var TaskRepository $taskRepository */
+        $taskRepository = static ::getContainer()->get(TaskRepository::class);
+        $task = $taskRepository->find(44);
+        $this->assertNull($task);
+
+    }
+
+
+
 
 }
